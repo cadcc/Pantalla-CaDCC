6780a4a82bf1572cc64aafb72930f3890c01642d
Francisco Madrid <fjmadrid@gmail.com>
2016-03-16 20:14:29 -0300

    refreshing screen each 2 hours

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,33 @@
  * Controller of the pantallaCaDccApp
  */
 angular.module('pantallaCaDccApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $element, $interval) {
+    var startColorChange = function () {
+      var screen = $($element);
+
+      screen.children().hide();
+
+      var colors = ['#FF0000', '#00FF00', '#0000FF'];
+      var count = 12;
+
+      var restoreColor = function () {
+        $interval.cancel($scope.blinkInterval);
+        screen.css('background-color', '#FFFFFF');
+        screen.children().show();
+      };
+
+      var changeColor = function () {
+        if (count === 0) {
+          restoreColor();
+          return;
+        }
+        screen.css('background-color', colors[count % colors.length]);
+        count -= 1;
+      };
+
+      $scope.blinkInterval = $interval(changeColor, 200);
+    };
+
+    // Each 2 hours
+    $interval(startColorChange, 2 * 3600 * 1000);
   });
