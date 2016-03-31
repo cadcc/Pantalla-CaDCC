b6d62e868c02293bf5ee37f082741aa661f53cf3
Francisco Madrid <fjmadrid@gmail.com>
2016-03-31 03:25:18 -0300

    code refactoring

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,38 @@
 'use strict';
+/*exported updateListInterval */
+
+var isList = function (obj) {
+  return Object.prototype.toString.call(obj) === '[object Array]';
+};
 
 var setWindowDimensions = function (scope) {
   scope.windowWidth = window.innerWidth;
   scope.windowHeight = window.innerHeight;
 };
 
-var updateListInterval = function (scope, dstKey, srcKey, pageSize) {
+var updateListInterval = function (scope, dstKey, srcKey, pageSize, filters) {
 
   var currentPage = -1;
 
   return function () {
     currentPage += 1;
 
-    if (currentPage >= parseInt(scope[srcKey].length / pageSize) + 1) {
+    var elements = scope[srcKey] || [];
+    if (isList(filters)) {
+      for (var i in filters) {
+        if (filters[i].type === 'date') {
+          elements = filters[i].func(moment(), elements);
+        }
+      }
+    }
+
+    if (currentPage >= Math.ceil(elements.length / pageSize)) {
       currentPage = 0;
     }
+
     var startIndex = (currentPage) * pageSize;
 
-    scope[dstKey] = scope[srcKey].slice(startIndex, startIndex + pageSize);
+    scope[dstKey] = elements.slice(startIndex, startIndex + pageSize);
   };
 };
 
--- app/scripts/directives/schedule.js
+++ app/scripts/directives/schedule.js
@@ -1,4 +1,5 @@
 'use strict';
+/*global updateListInterval */
 
 /**
  * @ngdoc function
@@ -23,16 +24,70 @@ angular.module('pantallaCaDccApp')
         updateTime();
         $interval(updateTime, 1000);
 
-        var updateEventsSlice = updateListInterval($scope, 'currentEvents', 'allEvents', pageSize);
-
         var makeDateWithTime = function (date_, timeStr) {
           return moment(date_.format('YYYY-MM-DD') + 'T' + timeStr, 'YYYY-MM-DDTHH:mm:ss');
         };
 
+        var compareDates = function (a, b) {
+          return function (date) {
+            return a.isSameOrBefore(date) && b.isAfter(date);
+          };
+        };
+
+        var sortEvents = function (events) {
+          // Order events by start date and code
+          return events.sort(function (a, b) {
+            if (a.fecha_ini === b.fecha_ini) {
+              return (a.codigo < b.codigo) ? -1 : (a.codigo > b.codigo) ? 1 : 0;
+            } else {
+              return (a.fecha_ini < b.fecha_ini) ? -1 : 1;
+            }
+          });
+        };
+
+        var filterEvents = {
+          type: 'date',
+          func: function (now, events) {
+
+            var startTime = makeDateWithTime(now, '08:00:00');
+            var filters = null;
+
+            for (var i = 0; i < 7; i++) {
+              var dateRange = {
+                startTime: angular.copy(startTime).add(2 * i, 'hours').subtract(30, 'minutes'),
+                endTime: angular.copy(startTime).add(2 * (i + 1), 'hours').subtract(30, 'minutes'),
+                filterStartTime: angular.copy(startTime).add(2 * i, 'hours'),
+                filterEndTime: angular.copy(startTime).add(2 * (i + 1), 'hours')
+              };
+
+              if (compareDates(dateRange.startTime, dateRange.endTime)(now)) {
+                filters = {
+                  start: dateRange.filterStartTime,
+                  end: dateRange.filterEndTime
+                };
+                break;
+              }
+            }
+
+            if (filters === null) {
+              return [];
+            }
+
+            var filteredEvents = events.filter(function (event) {
+              return compareDates(filters.start, filters.end)(moment(event.fecha_ini));
+            });
+
+            return sortEvents(filteredEvents);
+          }
+        };
+
+        var updateEventsSlice = updateListInterval($scope, 'currentEvents', 'allEvents', pageSize, [filterEvents]);
+
         var updateEvents = function () {
           var now = moment();
+          var url = 'https://www.u-cursos.cl/api/0/ingenieria/2/horario_institucion/agenda_diaria.json?departamento=5&fecha=' + now.format('YYYY-MM-DD');
 
-          $http.get('https://www.u-cursos.cl/api/0/ingenieria/2/horario_institucion/agenda_diaria.json?departamento=5&fecha=' + now.format('YYYY-MM-DD'))
+          $http.get(url)
             .then(function (response) {
               var jsonData = JSON.stringify(response.data);
 
@@ -44,47 +99,7 @@ angular.module('pantallaCaDccApp')
 
                 $scope.lastUpdate = now;
                 $scope.jsonData = jsonData;
-
-                var startTime = makeDateWithTime(now, '08:00:00');
-                var filters = {};
-
-                for (var i = 0; i < 7; i++) {
-                  var dateRange = {
-                    startTime: angular.copy(startTime).add(2 * i, 'hours').subtract(30, 'minutes'),
-                    endTime: angular.copy(startTime).add(2 * (i + 1), 'hours').subtract(30, 'minutes'),
-                    filterStartTime: angular.copy(startTime).add(2 * i, 'hours'),
-                    filterEndTime: angular.copy(startTime).add(2 * (i + 1), 'hours')
-                  };
-
-                  if (dateRange.startTime.isSameOrBefore(now) && dateRange.endTime.isAfter(now)) {
-                    filters = {
-                      start: dateRange.filterStartTime,
-                      end: dateRange.filterEndTime
-                    };
-                    break;
-                  }
-                }
-
-                if (Object.getOwnPropertyNames(filters).length === 0) {
-                  $scope.allEvents = [];
-                  return;
-                }
-
-                var events = response.data.filter(function (event) {
-                  var eventStartDate = moment(event.fecha_ini);
-                  return filters.start.isSameOrBefore(eventStartDate) && filters.end.isAfter(eventStartDate);
-                });
-
-                // Order events by start date and code
-                events = events.sort(function (a, b) {
-                  if (a.fecha_ini === b.fecha_ini) {
-                    return (a.codigo < b.codigo) ? -1 : (a.codigo > b.codigo) ? 1 : 0;
-                  } else {
-                    return (a.fecha_ini < b.fecha_ini) ? -1 : 1;
-                  }
-                });
-
-                $scope.allEvents = events;
+                $scope.allEvents = response.data;
 
                 updateEventsSlice();
                 $scope.eventsUpdatePromise = $interval(updateEventsSlice, 7500);
